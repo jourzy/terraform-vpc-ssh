5b69823bb5a712fce8c9217c97934640c2ab2da0
Jourzy <emmanora@hotmail.com>
2024-09-16 21:35:00 +0100

    Added the route_table_association resource which was missing and stopping me from being able to ssh into instance.

--- route.tf
+++ route.tf
@@ -11,3 +11,8 @@ resource "aws_route_table" "second" {
    Name = "2nd Route Table"
  }
 }
+
+resource "aws_route_table_association" "this" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.second.id
+}
