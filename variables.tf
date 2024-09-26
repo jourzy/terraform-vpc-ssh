f0d77d001877db60457970d83209a58e3e16d96a
Jourzy <emmanora@hotmail.com>
2024-09-26 11:02:07 +0100

    Removed unnecessary comments and added variable cidr_block_ingress. The value is stored in tfvars file and so must be entered during deployment.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-// default     = "ssh_key_pair"
+
 variable "key_pair_name" {
   description = "key_pair_name"
   type        = string
   default     = "public"
 }
 
-// default     = "t2.micro"
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   type        = string
@@ -18,28 +18,34 @@ variable "instance_tag" {
   default     = "instance"
 }
 
-// default     = 1
+
 variable "counter" {
   description = "Number of instances to launch"
   type        = number
   default     = 1
 }
 
-// default     = "public.pem"
+
 variable "file_name" {
   description = "Name of the key pair"
   type        = string
   default     = "public.pem"
 }
 
-// default       = "10.0.0.0/16"
+
 variable "cidr_block" {
   description = "CIDR Block"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-// eu-west-2a
+
+variable "cidr_block_ingress" {
+    description = "CIDR Block ingress"
+    type        = list(string)
+}
+
+
 variable "availability_zone"{
   description = "Availability Zones for the Subnet"
   type        = string
