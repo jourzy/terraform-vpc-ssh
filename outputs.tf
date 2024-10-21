9e0c8475c55d0de2074276385137795299e3d489
Jourzy <emmanora@hotmail.com>
2024-10-21 21:41:37 +0100

    Added the bastion public ip address to the output so I can use it for ssh into the private instances.

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,9 @@ output "public_subnet_ids" {
   // Gets the ids of all the aws_subnet resources that have the name public
   value       = aws_subnet.public[*].id
 }
+
+
+output "bastion_public_ip" {
+  description = "The public ip address of the bastion host"
+  value       = aws_instance.bastion.public_ip
+}
