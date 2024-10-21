3b112c3b75786a28a0f625dacf0f54c10cc47cc9
Jourzy <emmanora@hotmail.com>
2024-10-21 12:32:47 +0100

    Changed name to main.

--- key_pair.tf
+++ key_pair.tf
@@ -1,7 +1,7 @@
 
 #  Dont need this code as key-pair already exists
 
-# resource "aws_key_pair" "this" {
+# resource "aws_key_pair" "main" {
 #     key_name    = var.key_pair_name
 #     public_key  = file("~/.ssh/public.pem")
 
