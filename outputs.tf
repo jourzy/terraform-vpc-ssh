89585fb3af316906df57684c1c8c7a8fd828b234
Jourzy <emmanora@hotmail.com>
2024-10-21 13:26:56 +0100

    WORKING COMMIT. Added public subnet ids to the outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,12 @@
-output "instance_ip" {
-  description = "The public ip for ssh access"
-  value       = aws_instance.this[*].public_ip
+output "instance_public_ips" {
+  description = "The public ip addresses of the EC2 instances for ssh access"
+  // Maps the instance ids to their public ips
+  value       = {for instance in aws_instance.public : instance.id => instance.public_ip}
+}
+
+
+output "public_subnet_ids" {
+  description = "The ids of the public subnets to assign to EC2s"
+  // Gets the ids of all the aws_subnet resources that have the name public
+  value       = aws_subnet.public[*].id
 }
