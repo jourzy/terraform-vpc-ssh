1441221cb9fcb202368b0d309fe18cc5d32e241a
Jourzy <emmanora@hotmail.com>
2024-10-21 19:58:11 +0100

    Added nat gateway for public subnets. Added an elastic ip for the nat gateway.

--- gateway.tf
+++ gateway.tf
@@ -5,3 +5,18 @@ vpc_id = aws_vpc.main.id
    Name = "SSH"
  }
 }
+
+
+// NAT Gateways must be placed in public subnets to allow private instances to access the internet for updates, 
+// while still not being directly accessible from the internet.
+resource "aws_nat_gateway" "nat" {
+  count         = length(aws_subnet.public)
+
+  allocation_id = aws_eip.nat.id
+  subnet_id     = element(aws_subnet.public[*].id, count.index)  # Place the NAT gateway in the public subnets
+}
+
+// Allocate an Elastic IP (EIP) for the NAT gateway
+resource "aws_eip" "nat" {
+  vpc = true
+}
