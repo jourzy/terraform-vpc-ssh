6bf5e71782474892f7f80c35acdd2da36867b72b
Jourzy <emmanora@hotmail.com>
2024-10-21 19:50:20 +0100

    Changed length function to takethe public subnets as argument instead of the public cidr blocks.

--- route.tf
+++ route.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(var.cidr_blocks_subnet_public)
+  count          = length(aws_subnet.public)
   subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
