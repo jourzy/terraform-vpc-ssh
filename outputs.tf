3db5cdf27e2a931f07d956de556913d0126704f1
Jourzy <emmanora@hotmail.com>
2024-10-22 12:21:18 +0100

    Commented out the output for instance public ips as private isntances will not have public ips.

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "instance_public_ips" {
-  description = "The public ip addresses of the EC2 instances for ssh access"
-  // Maps the instance ids to their public ips
-  value       = {for instance in aws_instance.public : instance.id => instance.public_ip}
-}
+# output "instance_public_ips" {
+#   description = "The public ip addresses of the EC2 instances for ssh access"
+#   // Maps the instance ids to their public ips
+#   value       = {for instance in aws_instance.public : instance.id => instance.public_ip}
+# }
 
 
 output "public_subnet_ids" {
   description = "The ids of the public subnets to assign to EC2s"
-  // Gets the ids of all the aws_subnet resources that have the name public
+  # Gets the ids of all the aws_subnet resources that have the name public
   value       = aws_subnet.public[*].id
 }
 
