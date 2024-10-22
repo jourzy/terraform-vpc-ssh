899fcbc406ab9bf466271d1377e6afb5601bc651
Jourzy <emmanora@hotmail.com>
2024-10-22 15:23:10 +0100

    Added comments for readability.

--- route.tf
+++ route.tf
@@ -3,7 +3,7 @@
 resource "aws_route_table" "public" {
  vpc_id = aws_vpc.main.id
  
-
+# Routes all traffic through the internet gateway
  route {
    cidr_block = "0.0.0.0/0"
    gateway_id = aws_internet_gateway.main.id # Route through IG
